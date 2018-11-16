e3870808711ec3fc04359e74c7e70736258337ea
Marco (Hanrellz) Quadros <marcoaurelioquadros@gmail.com>
2018-11-16 20:31:29 -0200

    update poco version

--- install/InstallPoco.sh
+++ install/InstallPoco.sh
@@ -8,11 +8,11 @@ mkdir -p ~/tmp/poco
 
 cd ~/tmp/poco/
 
-wget https://pocoproject.org/releases/poco-1.7.8/poco-1.7.8p3-all.tar.gz
+wget https://pocoproject.org/releases/poco-1.7.9/poco-1.7.9-all.tar.gz
 
-tar -xvf poco-1.7.8p3-all.tar.gz
+tar -xvf poco-1.7.9-all.tar.gz
 
-cd poco-1.7.8p3-all
+cd poco-1.7.9-all
 
 mkdir cmake_build
 cd cmake_build
